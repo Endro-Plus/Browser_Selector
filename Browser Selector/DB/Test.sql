ac115dd88714ebc8ff8ba7c094b0f8d1214c8684
Bernard Ryan <lilbryan1018@gmail.com>
2025-12-12 16:32:11 -0500

    Delete Browser Selector directory

--- Browser Selector/DB/Test.sql
+++
@@ -1,4 +0,0 @@
---all of the necessary queries can be found here!
-
---SELECT browser_exe FROM browsers WHERE browser_name = "Chromium"
-SELECT browser_exe FROM bpos LEFT JOIN browsers ON bpos.browser_ID = browsers.browser_ID WHERE browser_icon = "..\IMG\Firefox.png";
